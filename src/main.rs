3c57c790e58582af779d59fde91d4b1f480e568e
Molly Draven <drazi@duck.com>
2024-05-17 08:27:41 -0400

    chore: Update Rocket.toml, Cargo.toml, and main.rs
    This commit adds the necessary configuration in Rocket.toml, updates the dependencies in Cargo.toml to include the Rocket crate, and modifies the main.rs file to include the necessary imports, routes, and error handling for the Rocket framework.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::Request;
+
+#[macro_use]
+extern crate rocket;
+
+#[derive(Debug)]
+struct Auth {
+    username: String,
+    password: String,
+}
+
+
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+    format!("Sorry, '{}' is not a valid path.", req.uri())
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/AuthLogin?<username>&<password>")]
+fn authlogin(username: String, password: String) -> String {
+    
+    let auth = Auth { username, password };
+    println!("{:?}", auth);
+    format!("Hello, {}! Your password is {}", auth.username, auth.password)
+
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build()
+        .mount("/", routes![index, authlogin])
+        .register("/", catchers![not_found])
+        .launch()
+        .await?;
+
+    Ok(())
 }
